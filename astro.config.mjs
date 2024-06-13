6d17ce2a5bd589fcb36c8b8a7149177da8fdad4d
Derek Drapp <dmdrapp@gmail.com>
2024-06-13 12:03:45 -0500

    publish to gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,5 @@ import solidJs from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs({ devtool: true })],
+  site: 'https://derekdrapp.github.io',
 });
