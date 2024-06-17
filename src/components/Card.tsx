a020abb40a602aba4d997441cb82277ed38363b9
Derek Drapp <dmdrapp@gmail.com>
2024-06-17 12:33:36 -0500

    Adding pages

---
+++ src/components/Card.tsx
@@ -0,0 +1,5 @@
+export default function Intro() {
+    return (
+        <p>Derek Drapp</p>
+    );
+}
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+export default function NavBar() {
+    return (
+        <div>
+            <a href="/">Home</a>
+            <a href="/about/">About</a>
+            <a href="/blog/">Blog</a>
+        </div>
+    );
+}
